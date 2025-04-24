6a6dd0508f8c6ec47686dc15cd2441d6e4d9bebf
1249215126 <19554104100@163.com>
2025-04-24 18:23:25 +0800

    再次重新，修复错误

--- assets/ios.transition-legacy-D3BFKWVN.js
+++ assets/ios.transition-legacy-D3BFKWVN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dflbustm.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.c,n=t.g}],execute:function(){
+System.register(["./index-legacy-Bn9DCVZ3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.c,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BBoE9Lps.js
+++ assets/swipe-back-legacy-BBoE9Lps.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dflbustm.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-Bn9DCVZ3.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
